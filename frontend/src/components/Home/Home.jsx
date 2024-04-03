27221f67c493d939c61aa5743626e4ebce8ced9a
adarsh22052003 <adarshsachanhiet@gmail.com>
2024-04-04 00:30:12 +0530

    Completing Home Page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import {Stack, 
+        VStack, 
+        Heading, 
+        Text,
+        Button, 
+        Image, 
+        Box, 
+        HStack } from "@chakra-ui/react";
+import "./home.css";
+import { Link } from "react-router-dom";
+import vg from "../../assests/images/bg.png";
+import {CgGoogle,CgYoutube} from "react-icons/cg";
+import {SiCoursera,SiUdemy} from "react-icons/si";
+import {DiAws} from "react-icons/di";
+import introVideo from "../../assests/videos/intro.mp4"
+
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <section className='home'>
+
+    <div className="container">
+      <Stack 
+      direction={["column","row"]} 
+      height={"100%"}
+      justifyContent={["center","space-between"]}
+      alignItems="center"
+      spacing={['16','56']}
+      >
+        <VStack width="full" alignItems={['center','flex-end']}>
+          <Heading children="LEARN FROM THE EXPERTS" size='xl'/>
+          <Text children="Find Valuable Content At Reasonable Price" />
+          <Link to="/courses">
+            <Button size={"lg"} colorScheme='yellow' >
+                Explore Now
+            </Button>
+          </Link>
+        </VStack>
+
+        <Image className='vector-graphics' boxSize={'md'} src={vg} objectFit={'contain'}></Image>
+      </Stack>
+    </div>
+
+    <Box padding={'8'} bg="blackAlpha.800">
+      <Heading 
+        textAlign={'center'} 
+        fontFamily={'body'} 
+        color={'yellow.400'} 
+        children="OUR BRANDS"/>
+        <HStack className="brandBanner" justifyContent={'space-evenly'} marginTop={'4'}>
+          <CgGoogle/>
+          <CgYoutube/>
+          <SiCoursera/>
+          <SiUdemy/>
+          <DiAws/>
+        </HStack>
+    </Box> 
+    <div className="container2">
+      <video
+        autoPlay
+        controls src={introVideo}
+        controlsList="nodownload nofullscreen noremoteplayback "
+        disablePictureInPicture
+        disableRemotePlayback
+       ></video>
+    </div>
+  </section>
 }
 
-export default Home
+export default Home;
